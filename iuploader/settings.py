49cb3679ccca7a8f2c59b40c8da96f9f6150476e
Muluken Gashaw <61051437+mule2020@users.noreply.github.com>
2022-06-05 17:12:16 +0530

    Update settings.py

--- iuploader/settings.py
+++ iuploader/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-z#@*$qab^5#w4fgw#hj+31lvi=2(_st**5h*ifv4gu2g_z9142'
+SECRET_KEY = '*****************************************************'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
